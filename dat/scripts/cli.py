52d1af4aa0e64d33cab5e09adf0dcfafffae2e3d
demorgan104 <timotei@tutanota.de>
2021-02-03 21:35:54 +0100

    added the new package structure

---
+++ dat/scripts/cli.py
@@ -0,0 +1,8 @@
+import click
+
+@click.command()
+def cli():
+    click.echo('Hello from DAT !')
+    
+
+
--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="package-api-demorgan104",
+setup(
+    name="dat",
     version="0.0.1",
     author="demorgan104",
     author_email="timotei@tutanota.de",
-    description="Package API with Conan IO",
+    description="The best friend of C/C++ developers",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/demorgan104/package-based-system",
-    packages=['package_api'],
+    url="https://github.com/demorgan104/dat",
+    packages=find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires = [
+        'Click'
+    ],
+    entry_points={
+        'console_scripts': [
+            'dat=dat.scripts.cli:cli'
+        ],
+    },
     python_requires='>=3.6',
 )
