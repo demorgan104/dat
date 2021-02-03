93fccdfc30b4d82fe85509a5b85e94577c84ef0f
demorgan104 <timotei@tutanota.de>
2021-02-03 21:50:28 +0100

    adding first cli commands

--- dat/scripts/cli.py
+++ dat/scripts/cli.py
@@ -1,8 +1,22 @@
 import click
 
-@click.command()
+@click.group()
 def cli():
-    click.echo('Hello from DAT !')
+    pass
+
+
+@cli.command()
+@click.option('--variant')
+def build(variant):
+    click.echo('Building variant %s' % variant)
+
+
+@cli.command()
+@click.option('--release-type')
+def release(release_type):
+    click.echo('Releasing %s' % release_type)
+
+
     
 
 
