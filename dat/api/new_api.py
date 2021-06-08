98eaccda2c9a4eb918bf39dd34d949d60784266e
demorgan104 <71926391+demorgan104@users.noreply.github.com>
2021-06-08 12:18:04 +0200

    Added new api (#10)
    * added new api
    
    * added the new api
    
    * fix requirements
    
    * fix requirements
    
    * update python version

---
+++ dat/api/new_api.py
@@ -0,0 +1,128 @@
+"""
+    Generates a completely functional DAT package
+"""
+import os
+import shutil
+import yaml
+from dat.errors.dat_exception import DatException
+
+class NewApi:
+    def __init__(self) -> None:
+        pass
+
+    def replace_name_in_content(self, content, name):
+        return content.replace(
+                '<${TEMPLATE_NAME}>',
+                name
+            )
+
+    def is_file_entry(self, value):
+        """Checks that an entry in the root is a file or not
+        Args:
+            value: Possible File content. This is always a list in case the file has a content.
+                   In case the file should be empty, the value will be None.
+        Returns:
+            If the key represents a file
+        """
+        if value == None:
+            return False
+        return 'None' in value or type(value) is list
+
+
+    def generate_file(self, file_name, content_identifier, root_dir, tags, template_name):
+        if content_identifier == 'None':
+            open(os.path.join(root_dir, file_name), 'w').close()
+        else:
+            # Use the content_identifier to get the content from top config
+            try:
+                tag = next(iter(content_identifier))
+                content = tags[tag]
+                file_path = os.path.join(root_dir, file_name)
+                if os.path.exists(file_path):
+                    os.remove(file_path)
+                with open(file_path, 'w') as file:
+                    content = self.replace_name_in_content(
+                        content, 
+                        template_name
+                    )
+                    file.write(content)
+            except KeyError:
+                self.session.logger.warning('Could not find {content_id}'.format(
+                    content_id=content_identifier
+                ))
+                open(os.path.join(root_dir, file_name), 'w').close()
+
+
+    def generate_structure(self, config, root_dir, tags, template_name):
+        for key, value in config.items():
+            if self.is_file_entry(value):
+                self.generate_file(
+                    key,
+                    value,
+                    root_dir,
+                    tags,
+                    template_name
+                )
+            else:
+                new_dir = os.path.join(root_dir, key)
+                os.mkdir(new_dir)
+                if not value is None:
+                    self.generate_structure(value, new_dir, tags, template_name)
+
+    def new(self, dest, name, forced=False):
+        """
+            TBD
+        """
+        print('Generating a DAT package')
+
+        template_file = os.path.join(os.path.dirname(__file__), '..', 'templates', 'basic.yml')
+        with open(template_file, 'r') as input_stream:
+            print(
+                'Loading template descriptor {descriptor_location}'.format(
+                    descriptor_location=template_file
+                )
+            )
+            descriptor_content = yaml.safe_load(
+                input_stream
+            )
+            
+        if not name:
+            raise DatException("Please provide a name for your package !")
+        # Create root dir
+        package_name = self.replace_name_in_content(descriptor_content['name'], name)
+        root_dir = os.path.join(dest, package_name)
+        self.original_root_dir = os.path.join(
+            dest,
+            package_name
+        )
+        if not os.path.exists(root_dir):
+            os.mkdir(root_dir)
+        else:
+            if forced:
+                print('Removing directory {dir} because it already exists !'.format(
+                    dir=root_dir
+                ))
+                shutil.rmtree(
+                    root_dir
+                )
+                os.mkdir(root_dir)
+            else:
+                raise DatException("The package you are trying to generate already exists !")
+
+        self.generate_structure(
+            descriptor_content['root'],
+            root_dir,
+            {
+                key: value for key, value in descriptor_content.items() if key != 'root'
+            },
+            name
+        )
+        print(
+            'Package generated at {destination}'.format(
+                destination=root_dir
+            )
+        )
+        if 'description' in descriptor_content:
+            print(
+                descriptor_content['description']
+            )
---
+++ dat/utils/replacements.py
@@ -0,0 +1,24 @@
+"""
+    A module that helps with template replacements
+"""
+
+import re
+
+def replace_and_create(replacements, template, dst):
+    """
+        TBD
+    """
+    
+    
+    pattern = re.compile(r"<\$(.*)>")
+    with open(template, "r") as template_file:
+        with open(dst, "w") as new_file:
+            for line in template_file:
+                match = re.search(pattern, line)
+                new_line = line
+                if match:
+                    token = match.group(1)
+                    new_line = line.replace(
+                        "<${token}>".format(token=token), str(replacements[token])
+                    )
+                new_file.write(new_line)
