86592089f18c24dcc00270b48c954b61290444d2
demorgan104 <71926391+demorgan104@users.noreply.github.com>
2021-08-01 10:51:02 +0200

    Complete dat workflow (#20)
    * complete dat workflow
    
    * solve pylint issues

---
+++ dat/api/document_api.py
@@ -0,0 +1,33 @@
+"""
+    DAT package test API
+"""
+import os
+import subprocess
+import webbrowser
+from dat.utils.dat_logger import app_logger
+from dat.errors.dat_exception import DatException
+
+
+class DocumentApi:
+    """
+    TBD
+    """
+
+    # pylint: disable=too-few-public-methods
+    def __init__(self) -> None:
+        self.current_dir = os.getcwd()
+
+    def document(self):
+        """
+        Document api implementation
+        """
+        app_logger.info("Document api")
+        doc_dir = os.path.join(self.current_dir, 'doc')
+        os.chdir(doc_dir)
+        mkdocs_cmd = ['mkdocs', 'serve']
+        try:
+            webbrowser.open('http://localhost:8000/')
+            subprocess.run(mkdocs_cmd, check=True)
+        except subprocess.CalledProcessError as error:
+            app_logger.error("Error executing %s", mkdocs_cmd, exc_info=True)
+            raise DatException(error) from error
---
+++ dat/api/run_api.py
@@ -0,0 +1,27 @@
+"""
+    DAT package test API
+"""
+import os
+from dat.errors.dat_exception import DatException
+
+
+class RunApi:
+    """
+    TBD
+    """
+
+    # pylint: disable=too-few-public-methods
+    def __init__(self) -> None:
+        self.current_dir = os.getcwd()
+
+    # pylint: disable=R1732,W0122
+    def run(self):
+        """
+        Document api implementation
+        """
+        extensions_dir = os.path.join(self.current_dir, 'extensions')
+        run_extensions = os.path.join(extensions_dir, "run.py")
+        if not os.path.exists(run_extensions):
+            raise DatException("You should place a run.py script inside extensions folder !")
+        exec(open(run_extensions).read())
+        #run_app()
